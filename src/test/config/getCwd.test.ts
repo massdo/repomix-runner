f64c5a798633a844addea5bd97b29cabf9a5fd91
Dorian Massoulier <massoulier.d@gmail.com>
2025-01-14 19:46:25 +0100

    feat: finish getCwd

--- src/test/config/getCwd.test.ts
+++ src/test/config/getCwd.test.ts
@@ -1,11 +1,28 @@
 import * as assert from 'assert';
 import { getCwd } from '../../config/getCwd';
 import { basename } from 'path';
+import * as vscode from 'vscode';
+import * as sinon from 'sinon';
 
-/**
- * Test to verify that the getCwd function returns the path of the root test workspace used for tests.
- */
-test('should return the root test workspace folder path', () => {
-  const cwd = basename(getCwd());
-  assert.strictEqual(basename(cwd), 'root');
+suite('getCwd', () => {
+  /**
+   * Test to ensure that the function returns the root test workspace folder path.
+   */
+  test('should return the root test workspace folder path', () => {
+    const cwd = basename(getCwd());
+    assert.strictEqual(basename(cwd), 'root');
+  });
+
+  /**
+   * Test to ensure that the function throws an error when no workspace folder is found.
+   */
+  test('should throw error when no workspace folder is found', () => {
+    const workspaceFoldersMock = sinon
+      .stub(vscode.workspace, 'workspaceFolders')
+      .get(() => undefined);
+    assert.throws(() => {
+      getCwd();
+    }, /No root folder/);
+    workspaceFoldersMock.restore();
+  });
 });
